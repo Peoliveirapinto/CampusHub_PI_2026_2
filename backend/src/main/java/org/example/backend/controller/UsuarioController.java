eb694b1d1e305c1b5740af2bf89e6c6cda52fcbe
Matheus Ming <minggiongo11@gmail.com>
2026-09-18 08:28:27 -0300

    feat(controller): Cria o arquivo UsuarioController
    - Cria o arquivo de controller para a entidade Usuario, estrutura pronta para adição dos métodos necessários no futuro

---
+++ backend/src/main/java/org/example/backend/controller/UsuarioController.java
@@ -0,0 +1,13 @@
+package org.example.backend.controller;
+
+import org.example.backend.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+}
