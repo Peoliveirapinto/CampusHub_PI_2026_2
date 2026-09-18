b1fde70178d5e7a051e41ec0988a30f9cee06ed1
Matheus Ming <minggiongo11@gmail.com>
2026-09-18 08:17:15 -0300

    feat(repository): Cria o arquivo UsuarioRepository
    - Cria o arquivo de repository para a entidade Usuario, já está pronto

---
+++ backend/src/main/java/org/example/backend/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package org.example.backend.repository;
+
+import org.example.backend.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+}
