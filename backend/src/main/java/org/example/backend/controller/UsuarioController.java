58b4a2d02fe4d342868dcd4be8301b328481d13c
Matheus Ming <minggiongo11@gmail.com>
2026-09-18 10:07:32 -0300

    feat(api): implementação da funcionalidade de GET para entidade Usuario
    - funcionalidade implementada na camada de controller e de service

--- backend/src/main/java/org/example/backend/controller/UsuarioController.java
+++ backend/src/main/java/org/example/backend/controller/UsuarioController.java
@@ -1,13 +1,26 @@
 package org.example.backend.controller;
 
+import org.example.backend.model.Usuario;
 import org.example.backend.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable UUID id){
+        Usuario usuario = usuarioService.getById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(usuario);
+    }
 }
--- backend/src/main/java/org/example/backend/service/UsuarioService.java
+++ backend/src/main/java/org/example/backend/service/UsuarioService.java
@@ -1,11 +1,21 @@
 package org.example.backend.service;
 
+import org.example.backend.exception.ResourceNotFoundException;
+import org.example.backend.model.Usuario;
 import org.example.backend.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    public Usuario getById(UUID id){
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Usuário não encontrado com id: " + id)
+        );
+    }
 }
